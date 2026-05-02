e22d063e22f9daeeb0d1f8933b11fe7be3a62a80
Akramov Abubakr <abuaqu647@gmail.com>
2026-05-02 17:40:46 +0500

    checked finished work

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -4,44 +4,44 @@ var work = true;
 while (work)
 {
     Console.WriteLine();
-    Console.WriteLine("1. Добавить книгу");
-    Console.WriteLine("2. Показать все книги");
-    Console.WriteLine("3. Обновить книгу");
-    Console.WriteLine("4. Удалить книгу");
-    Console.WriteLine("5. Поиск по названию");
-    Console.WriteLine("6. Поиск по автору");
-    Console.WriteLine("7. Выход");
+    Console.WriteLine("1-Добавить книгу");
+    Console.WriteLine("2-Показать все книги");
+    Console.WriteLine("3-Обновить книгу");
+    Console.WriteLine("4-Удалить книгу");
+    Console.WriteLine("5-Поиск по названию");
+    Console.WriteLine("6-Поиск по автору");
+    Console.WriteLine("7-Выход");
     Console.Write("Выберите пункт: ");
 
-    string num = Console.ReadLine()!;
+    string number = Console.ReadLine()!;
 
     try
     {
-        if (num == "1")
+        if (number == "1")
         {
             Library.AddBook(library);
         }
-        else if (num == "2")
+        else if (number == "2")
         {
             library.DisplayBooks();
         }
-        else if (num == "3")
+        else if (number == "3")
         {
             Library.UpdateBook(library);
         }
-        else if (num == "4")
+        else if (number == "4")
         {
             Library.DeleteBook(library);
         }
-        else if (num == "5")
+        else if (number == "5")
         {
             Library.SearchByTitle(library);
         }
-        else if (num == "6")
+        else if (number == "6")
         {
             Library.SearchByAuthor(library);
         }
-        else if (num == "7")
+        else if (number == "7")
         {
             work = false;
             Console.WriteLine("Программа завершена.");
